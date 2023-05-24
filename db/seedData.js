f307b0084dfbd639201f2b9cda922be66bb522cc
Matthew Rowley <mattrowley10@gmail.com>
2023-05-24 10:27:10 -0600

    some dummy data

--- db/seedData.js
+++ db/seedData.js
@@ -1,7 +1,14 @@
-async function createTables(){
-    try{
-        
-    } catch (error){
-        throw (error)
-    }
-}
+const users = [
+    { username: "Lindsay", password: 12345678 },
+    { username: "Matthew", password: 12345678 },
+]
+
+const routines = [
+    { id: { id }, creator_id: 1, is_public: true, name: 'Matthew', goal: "to be the best"},
+    { id: { id }, creator_id: 2, is_public: false, name: 'Lindsay', goal: "to win!"}
+]
+
+const activities = [
+    { id: { id }, name: "Matthew", description: "leg day"},
+    { id: { id }, name: "Lindsay", description: "push-ups"},
+]
