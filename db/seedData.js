12350f45e353cc1e4242ce17056f3478de49c4ee
LindsayBrumley <lalainaphillips@gmail.com>
2023-05-24 14:58:01 -0400

    test

---
+++ db/seedData.js
@@ -0,0 +1,26 @@
+const users = [
+  { username: "Lindsay", password: 12345678 },
+  { username: "Matthew", password: 12345678 },
+];
+
+const routines = [
+  {
+    id: { id },
+    creator_id: 1,
+    is_public: true,
+    name: "Matthew",
+    goal: "to be the best",
+  },
+  {
+    id: { id },
+    creator_id: 2,
+    is_public: false,
+    name: "Lindsay",
+    goal: "to win!",
+  },
+];
+
+const activities = [
+  { id: { id }, name: "Matthew", description: "leg day" },
+  { id: { id }, name: "Lindsay", description: "push-ups" },
+];
