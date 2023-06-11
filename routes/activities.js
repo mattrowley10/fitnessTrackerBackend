9f91d188d1bfdaa3a999984519c3bfeba1ac8b6a
LindsayBrumley <lalainaphillips@gmail.com>
2023-06-11 11:43:57 -0400

    oops working on main branch, committing some routes in activities

--- routes/activities.js
+++ routes/activities.js
@@ -2,10 +2,12 @@ const activitiesRouter = require("express").Router();
 const {
   getAllActivities,
   getActivitiesById,
+  createActivities,
 } = require("../db/adapters/activities");
+const { getPublicRoutinesByActivity } = require("../db/adapters/routines");
 const { authRequired } = require("./utils");
 
-activitiesRouter.get("/", authRequired, async (req, res, next) => {
+activitiesRouter.get("/activities", authRequired, async (req, res, next) => {
   try {
     const activities = await getAllActivities();
     res.send(activities);
@@ -13,7 +15,7 @@ activitiesRouter.get("/", authRequired, async (req, res, next) => {
     next(error);
   }
 });
-activitiesRouter.get("/:id", authRequired, async (req, res, next) => {
+activitiesRouter.get("/:activityid", authRequired, async (req, res, next) => {
   try {
     const id = req.params.id;
     const activitiesById = await getActivitiesById(id);
@@ -25,11 +27,32 @@ activitiesRouter.get("/:id", authRequired, async (req, res, next) => {
   }
 });
 
-activitiesRouter.post("/create-activity", authRequired, (req, res, next) => {
-  try {
-    const { name, description } = req.body;
-  } catch (error) {
-    next(error);
+activitiesRouter.post(
+  "/create-activity",
+  authRequired,
+  async (req, res, next) => {
+    try {
+      const { name, description } = req.body;
+      const activity = await createActivities({ name, description });
+      req.send(activity);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
+
+activitiesRouter.get(
+  "/:activityId/routines",
+  authRequired,
+  async (req, res, next) => {
+    try {
+      const { activity_id } = req.params;
+      const activity = await getActivitiesById(activity_id);
+      const routines = await getPublicRoutinesByActivity(activity_id);
+      req.send({ activity, routines });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 module.exports = activitiesRouter;
