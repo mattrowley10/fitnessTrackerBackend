9ed13423c3b53496e3770a6b7c6ef9b36b695a92
LindsayBrumley <lalainaphillips@gmail.com>
2023-06-11 21:46:24 -0400

    fixed

--- client/src/components/activities.jsx
+++ client/src/components/activities.jsx
@@ -1,41 +1,7 @@
 // // import React from 'react'
 // // import { useNavigate } from "react-router"
-// export default function Activities() {
-//   const [activities, setActivities] = useState([]);
-//   useEffect(()=>{
-//     async function
-//   })
-//   return <div>activities</div>;
-// }
-// import { useState, useEffect } from "react";
-// import { getAllRoutines } from "../api/helpers";
-
-// export default function Routines() {
-//   const [routines, setRoutines] = useState([]);
-//   useEffect(() => {
-//     async function allRoutines() {
-//       try {
-//         const allRoutines = await getAllRoutines();
-//         setRoutines(allRoutines);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     }
-//     allRoutines();
-//   }, []);
-
-//   return (
-//     <div className="home-page">
-//       {routines.map((routine) => {
-//         return (
-//           <div className="one-routine" key={routine.id}>
-//             <h2 className="routine-name">{routine.name}</h2>
-//             <ul className="routine-info">
-//               <li>{routine.description}</li>
-//             </ul>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// }
+export default function Activities() {
+  return <div>activities</div>;
+}
+import { useState, useEffect } from "react";
+import { getAllRoutines } from "../api/helpers";
